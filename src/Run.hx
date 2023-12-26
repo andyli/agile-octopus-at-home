552ca6c57a83ae02a455bcad99a72c3426d23c43
Andy Li <andy@onthewings.net>
2023-12-26 01:37:03 +0000

    implement schedule-next-day

--- src/Run.hx
+++ src/Run.hx
@@ -1,7 +1,13 @@
+import js.lib.Date as JsDate;
 import sys.io.Process;
 import sys.io.File;
 import haxe.io.Path;
 using DateTools;
+
+enum TaskType {
+    TStart;
+    TStop;
+}
 
 class Run {
     static function cmd(command:String, args:Array<String>):Void {
@@ -31,6 +37,7 @@ class Run {
     static function main():Void {
         final scriptDir = Path.directory(Sys.programPath());
         Sys.setCwd(scriptDir);
+        js.Lib.require('dotenv').config();
         final logFile = Path.join(["logs", Date.now().format("%Y-%m-%d_%H-%M-%S") + ".log"]);
         switch (Sys.args()) {
             case []:
@@ -67,6 +74,58 @@ class Run {
                         }
                     )
                     .then(o -> trace(o));
+            case ["schedule-next-day"]:
+                final now = Date.now();
+                new OctopusEnergyApi(Sys.getEnv("OCTOPUS_ENERGY_API_KEY"))
+                    .getElectricityStandardUnitRates(
+                        Sys.getEnv("OCTOPUS_ENERGY_PRODUCT_CODE"),
+                        Sys.getEnv("OCTOPUS_ENERGY_TARIFF_CODE"),
+                        {
+                            periodFrom: now,
+                            periodTo: now.delta(DateTools.days(1)),
+                        }
+                    )
+                    .then(o -> {
+                        final rates = o.results
+                            .map(r -> {
+                                from: JsDate.toHaxeDate(new JsDate(r.valid_from)),
+                                to: JsDate.toHaxeDate(new JsDate(r.valid_to)),
+                                rate: r.value_inc_vat,
+                            })
+                            .filter(r -> r.from.getTime() > now.getTime());
+                        rates.sort((a, b) -> a.from.getTime() - b.from.getTime() > 0 ? 1 : -1);
+                        // trace(rates);
+                        rates;
+                    })
+                    .then(rates -> {
+                        final tasks:Array<{ type:TaskType, at:Date }> = [];
+                        for (r in rates) {
+                            switch (tasks[tasks.length-1]) {
+                                case null | { type: TStop, at: _ }:
+                                    if (r.rate < 0) {
+                                        tasks.push({ type: TStart, at: r.from });
+                                    }
+                                case { type: TStart, at: _ }:
+                                    if (r.rate > 0) {
+                                        tasks.push({ type: TStop, at: r.from });
+                                    }
+                            }
+                        }
+                        // trace(tasks);
+                        tasks;
+                    })
+                    .then(tasks -> {
+                        for (t in tasks) {
+                            switch (t.type) {
+                                case TStart:
+                                    final taskName = "agile-octopus-at-home_" + t.at.format("%Y-%m-%d_%H-%M") + "_start";
+                                    scheduleRunOnce(t.at, taskName, ["start", taskName]);
+                                case TStop:
+                                    final taskName = "agile-octopus-at-home_" + t.at.format("%Y-%m-%d_%H-%M") + "_stop";
+                                    scheduleRunOnce(t.at, taskName, ["stop", taskName]);
+                            }
+                        }
+                    });
             case _:
                 throw "unexpected args";
         }
